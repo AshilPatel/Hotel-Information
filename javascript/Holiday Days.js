e9eccf7a5257d1437895dcc8092005005ab9f2cb
AshilPatel <65237607+AshilPatel@users.noreply.github.com>
2022-07-28 09:37:50 +0530

    Add files via upload

---
+++ javascript/Holiday Days.js
@@ -0,0 +1,320 @@
+function fun()
+{
+    var mywindow=window.open("Holiday Days.html","_self");
+}
+
+/*function fun1()
+{
+    var mywindow2=window.open("Holiday Gallery.html","_self");
+}*/
+function fun2()
+{
+    var mywindow3=window.open("Holiday Contact Us.html","_self");
+}
+function fun3()
+{
+    var mywindow3=window.open("Holiday About.html","_self");
+}
+// function fun4()
+// {
+//     var mywindow4=window.open("login.html");
+// }
+// function fun401()
+// {
+//     var mywindow5=window.open("Registration (1).html","_self");
+// }
+
+function fun31() {
+    var dots = document.getElementById("dots");
+    var moreText = document.getElementById("more");
+    var btnText = document.getElementById("myBtn");
+  
+    if (dots.style.display === "none") {
+      dots.style.display = "inline";
+      btnText.innerHTML = "Read more"; 
+      moreText.style.display = "none";
+    } else {
+      dots.style.display = "none";
+      btnText.innerHTML = "Read less"; 
+      moreText.style.display = "inline";
+    }
+}
+
+function fun11()
+{
+    var mywindow11=window.open("Holiday Gallery(Mumbai).html","_self");
+}
+function fun12()
+{
+    var mywindow11=window.open("Holiday Gallery(Goa).html","_self");
+}
+function fun13()
+{
+    var mywindow11=window.open("Holiday Gallery(Gujarat).html","_self");
+}
+function fun14()
+{
+    var mywindow11=window.open("Holiday Gallery(Himachal Pradesh).html","_self");
+}
+function fun15()
+{
+    var mywindow11=window.open("Holiday Gallery(Delhi).html","_self");
+}
+function fun16()
+{
+    var mywindow11=window.open("Holiday Gallery(Rajasthan).html","_self");
+}
+function fun21()
+{
+    var mywindow11=window.open("Holiday Contact(Mumbai).html","_self");
+}
+function fun22()
+{
+    var mywindow11=window.open("Holiday Contact(Goa).html","_self");
+}
+function fun23()
+{
+    var mywindow11=window.open("Holiday Contact(Gujarat).html","_self");
+}
+function fun24()
+{
+    var mywindow11=window.open("Holiday Contact(Himachal Pradesh).html","_self");
+}
+function fun25()
+{
+    var mywindow11=window.open("Holiday Contact(Delhi).html","_self");
+}
+function fun26()
+{
+    var mywindow11=window.open("Holiday Contact(Rajasthan).html","_self");
+}
+function fun100()
+{
+    var mywindow11=window.open("https://www.facebook.com/");
+}
+function fun101()
+{
+    var mywindow11=window.open("https://twitter.com/");
+}
+function fun102()
+{
+    var mywindow11=window.open("https://www.instagram.com/");
+}
+
+
+
+
+
+
+function fun500()
+{
+    var mywindow11=window.open("Home family mumbai.html","_self");
+}
+function fun501()
+{
+    var mywindow11=window.open("Home family goa.html","_self");
+}
+function fun502()
+{
+    var mywindow11=window.open("Home family Gujarat.html","_self");
+}
+function fun503()
+{
+    var mywindow11=window.open("Home Family Karnataka.html","_self");
+}
+function fun504()
+{
+    var mywindow11=window.open("Home Family Delhi.html","_self");
+}
+function fun505()
+{
+    var mywindow11=window.open("Home Family Rajasthan.html","_self");
+}
+
+
+// $(window).on("scroll", function() {
+//     if($(window).scrollTop() > 8) {
+//         $(".navbar").addClass("change");
+//     } else {
+//         //remove the background property so it comes transparent again (defined in your css)
+//        $(".navbar").removeClass("change");
+//     }
+// });
+
+
+
+
+
+
+
+//hotel Mumbai
+
+
+function fun1000()
+{
+    var mywindow11=window.open("Mumbai itc(hotel mumbai).html","_self");
+}
+function fun1001()
+{
+    var mywindow11=window.open("Mumbai taj(hotel mumbai).html","_self");
+}
+function fun1002()
+{
+    var mywindow11=window.open("Mumbai grand hyatt(hotel mumbai).html","_self");
+}
+function fun1003()
+{
+    var mywindow11=window.open("Mumbai bandra(hotel mumbai).html","_self");
+}
+function fun1004()
+{
+    var mywindow11=window.open("Mumbai JW(hotel mumbai).html","_self");
+}
+function fun1005()
+{
+    var mywindow11=window.open("Mumbai leela(hotel mumbai).html","_self");
+}
+
+
+//hotel Goa
+
+
+function fun2000()
+{
+    var mywindow11=window.open("Goa itc(hotel goa).html","_self");
+}
+function fun2001()
+{
+    var mywindow11=window.open("Goa taj(hotel goa).html","_self");
+}
+function fun2002()
+{
+    var mywindow11=window.open("Goa grand hyatt(hotel goa).html","_self");
+}
+function fun2003()
+{
+    var mywindow11=window.open("Goa Hiton(hotel goa).html","_self");
+}
+function fun2004()
+{
+    var mywindow11=window.open("Goa W(hotel goa).html","_self");
+}
+function fun2005()
+{
+    var mywindow11=window.open("Goa leela(hotel goa).html","_self");
+}
+
+
+//hotel Gujarat
+
+
+function fun3000()
+{
+    var mywindow11=window.open("Gujarat itc(hotel guj).html","_self");
+}
+function fun3001()
+{
+    var mywindow11=window.open("Gujarat taj(hotel guj).html","_self");
+}
+function fun3002()
+{
+    var mywindow11=window.open("Gujarat grand hyatt(hotel guj).html","_self");
+}
+function fun3003()
+{
+    var mywindow11=window.open("Gujarat Hiton(hotel guj).html","_self");
+}
+function fun3004()
+{
+    var mywindow11=window.open("Gujarat Hawthorn(hotel guj).html","_self");
+}
+function fun3005()
+{
+    var mywindow11=window.open("Gujarat Marriott(hotel guj).html","_self");
+}
+
+
+//hotel Himanchal pradesh
+
+
+function fun4000()
+{
+    var mywindow11=window.open("Karnataka itc(hotel ka).html","_self");
+}
+function fun4001()
+{
+    var mywindow11=window.open("Karnataka taj(hotel ka).html","_self");
+}
+function fun4002()
+{
+    var mywindow11=window.open("karnataka hyatt(hotel ka).html","_self");
+}
+function fun4003()
+{
+    var mywindow11=window.open("Karnataka naveen(hotel ka).html","_self");
+}
+function fun4004()
+{
+    var mywindow11=window.open("Karnataka marriott(hotel ka).html","_self");
+}
+function fun4005()
+{
+    var mywindow11=window.open("Karnataka leela(hotel ka).html","_self");
+}
+
+//hotel Delhi
+
+
+function fun5000()
+{
+    var mywindow11=window.open("Delhi itc(hotel delhi).html","_self");
+}
+function fun5001()
+{
+    var mywindow11=window.open("Delhi taj(hotel delhi).html","_self");
+}
+function fun5002()
+{
+    var mywindow11=window.open("Delhi hyatt(hotel delhi).html","_self");
+}
+function fun5003()
+{
+    var mywindow11=window.open("Delhi hilton(hotel delhi).html","_self");
+}
+function fun5004()
+{
+    var mywindow11=window.open("Delhi eros(hotel delhi).html","_self");
+}
+function fun5005()
+{
+    var mywindow11=window.open("Delhi leela(hotel delhi).html","_self");
+}
+
+
+//hotel Rajasthan
+
+
+function fun6000()
+{
+    var mywindow11=window.open("Rajasthan itc(hotel raj).html","_self");
+}
+function fun6001()
+{
+    var mywindow11=window.open("Rajasthan taj(hotel raj).html","_self");
+}
+function fun6002()
+{
+    var mywindow11=window.open("Rajasthan hyatt(hotel raj).html","_self");
+}
+function fun6003()
+{
+    var mywindow11=window.open("Rajasthan hilton(hotel Raj).html","_self");
+}
+function fun6004()
+{
+    var mywindow11=window.open("Rajasthan Marriott(hotel raj).html","_self");
+}
+function fun6005()
+{
+    var mywindow11=window.open("Rajasthan leela(hotel raj).html","_self");
+}
